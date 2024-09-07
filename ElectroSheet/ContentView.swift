19deed231c9a8c5bc85090b8e44eaafd004a58dc
DJShabsifhe <zhangbaole111@126.com>
2024-09-07 10:20:54 +0800

    Initial Commit

---
+++ ElectroSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElectroSheet
+//
+//  Created by DJShabsifhe on 2024/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElectroSheet/ElectroSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroSheetApp.swift
+//  ElectroSheet
+//
+//  Created by DJShabsifhe on 2024/9/7.
+//
+
+import SwiftUI
+
+@main
+struct ElectroSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
